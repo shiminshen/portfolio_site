ad2d5210306f72fbe9f2989c86962383f1c5cb44
Damon Shen <shi-min.shen@oath.com>
2018-08-04 17:56:55 +0800

    deploy to gh-pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
+
+const Wrapper = styled.div`
+  background: #ff0;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Wrapper className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,7 +18,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </Wrapper>
     );
   }
 }
